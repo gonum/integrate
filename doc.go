3c7b042e7cd6f1dcb8c130f5c31d88a363c02f58
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2018-12-09 16:07:09 +0100

    integrate: add deprecation notice

--- doc.go
+++ doc.go
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// This repository is no longer maintained.
+// Development has moved to https://github.com/gonum/gonum.
+//
 // Package integrate provides functions to compute an integral given a
 // specific list of evaluations.
 package integrate // "github.com/gonum/integrate"
